0cab92b902764c5a6f49f4c4ff26e36fea4f1d06
Theodore DeRego <tderego94@gmail.com>
2015-10-05 18:03:57 -1000

    implemented argparse

--- calculate_gc_content.py
+++ calculate_gc_content.py
@@ -3,13 +3,23 @@
 # Command line script to calculate base frequency from a .fasta file
 
 import sys
+import argparse
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("fasta_file")
+args = parser.parse_args()
+
+###########################################################
+
+fasta = args.fasta_file
 
 a_count = 0
 c_count = 0
 g_count = 0
 t_count = 0
 
-with open(sys.argv[1], 'r') as fasta:
+with open(fasta, 'r') as fasta:
     for line in fasta:
         if line.startswith(">"):
             # Header line, skip it
@@ -24,7 +34,7 @@ with open(sys.argv[1], 'r') as fasta:
             else:
                 t_count += 1
 
-print("Base counts for file %s:" % sys.argv[1])
+print("Base counts for file %s:" % fasta)
 print("A: %d" % a_count)
 print("C: %d" % c_count)
 print("G: %d" % g_count)
